09b061dfd601055ea5fb894a897e4c066b704a52
neel <neelkandlikar@gmail.com>
2019-03-02 09:30:43 -0800

    added Utils

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+      String output =   Utils.readFileAsString("data/Education.csv");
+        System.out.println(output);
+
+    }
 }
